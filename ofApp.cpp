9329761d41f73b230250bbb0540b2bf735bbb079
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2024-07-05 18:37:58 +0900

    Add files via upload

---
+++ ofApp.cpp
@@ -0,0 +1,111 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	ofSetFrameRate(25);
+	ofSetWindowTitle("openFrameworks");
+
+	ofBackground(39);
+	ofSetLineWidth(1.5);
+	ofSetCircleResolution(60);
+	ofEnableBlendMode(ofBlendMode::OF_BLENDMODE_ADD);
+
+	for (int i = 0; i < 8; i++) {
+
+		this->noise_seed_list.push_back(glm::vec2(ofRandom(1000), ofRandom(1000)));
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+	int index = 0;
+	auto radius = 20;
+	ofColor color;
+	for (auto& noise_seed : this->noise_seed_list) {
+
+		auto deg = ofGetFrameNum() * 3 + index * 45;
+		auto next_deg = deg + 3;
+
+		auto location = glm::vec2(radius * cos(deg * DEG_TO_RAD), radius * sin(deg * DEG_TO_RAD));
+		auto next = glm::vec2(radius * cos(next_deg * DEG_TO_RAD), radius * sin(next_deg * DEG_TO_RAD));
+
+		auto distance = location - next;
+		distance *= 1;
+
+		for (int i = 0; i < 2; i++) {
+
+			auto future = location + distance * 8;
+			auto random_deg = ofRandom(360);
+			future += glm::vec2(15 * cos(random_deg * DEG_TO_RAD), 15 * sin(random_deg * DEG_TO_RAD));
+			auto future_distance = future - location;
+
+			this->location_list.push_back(location);
+			this->velocity_list.push_back(glm::normalize(future_distance) * glm::length(distance));
+
+			color.setHsb(ofMap(index, 0, this->noise_seed_list.size(), 0, 255), 255, 255);
+			this->color_list.push_back(color);
+		}
+
+		index++;
+	}
+
+	for (int i = this->location_list.size() - 1; i > -1; i--) {
+
+		this->location_list[i] += this->velocity_list[i];
+		this->velocity_list[i] *= 1.01;
+
+		if (glm::distance(glm::vec2(), this->location_list[i]) > 1000) {
+
+			this->location_list.erase(this->location_list.begin() + i);
+			this->velocity_list.erase(this->velocity_list.begin() + i);
+			this->color_list.erase(this->color_list.begin() + i);
+		}
+	}
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	ofTranslate(ofGetWindowSize() * 0.5);
+	ofRotateY(180);
+
+	int index = 0;
+	for (auto& location : this->location_list) {
+
+		ofSetColor(this->color_list[index++], 32);
+
+		ofPushMatrix();
+		ofTranslate(location);
+
+		auto size = ofMap(glm::length(location), 120, 360, 3, 42);
+		ofDrawCircle(glm::vec2(), size);
+
+		ofPopMatrix();
+	}
+
+	/*
+	int start = 500;
+	if (ofGetFrameNum() > start) {
+
+		ostringstream os;
+		os << setw(4) << setfill('0') << ofGetFrameNum() - start;
+		ofImage image;
+		image.grabScreen(0, 0, ofGetWidth(), ofGetHeight());
+		image.saveImage("image/cap/img_" + os.str() + ".jpg");
+		if (ofGetFrameNum() - start >= 25 * 20) {
+
+			std::exit(1);
+		}
+	}
+	*/
+}
+
+//--------------------------------------------------------------
+int main() {
+
+	ofSetupOpenGL(720, 720, OF_WINDOW);
+	ofRunApp(new ofApp());
+}
---
+++ ofApp.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp {
+
+public:
+	void setup();
+	void update();
+	void draw();
+
+	void keyPressed(int key) {};
+	void keyReleased(int key) {};
+	void mouseMoved(int x, int y) {};
+	void mouseDragged(int x, int y, int button) {};
+	void mousePressed(int x, int y, int button) {};
+	void mouseReleased(int x, int y, int button) {};
+	void windowResized(int w, int h) {};
+	void dragEvent(ofDragInfo dragInfo) {};
+	void gotMessage(ofMessage msg) {};
+
+	vector<glm::vec2> noise_seed_list;
+	vector<glm::vec2> location_list;
+	vector<glm::vec2> velocity_list;
+	vector<ofColor> color_list;
+};
